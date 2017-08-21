43aa2521c6ee9a8b1ef78a16320f45618466584f
heymitchfischer <mitchfischer6@gmail.com>
2017-08-21 18:33:56 -0500

    added recipe instructions

--- recipe.rb
+++ recipe.rb
@@ -1,1 +1,5 @@
-PB&J Recipe
+PB&J Recipe:
+1) Get some bread, peanut butter, and jelly
+2) Put peanut butter on one slice of bread
+3) Put jelly on other slice of bread
+4) Put bread slices together
